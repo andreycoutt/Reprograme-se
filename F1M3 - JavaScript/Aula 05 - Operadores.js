c341ddd0e5d202a32306ae0c910c0d4bbe742120
andreycoutt <contato.andreyyy@gmail.com>
2023-09-24 19:30:20 -0300

    Operadores

---
+++ F1M3 - JavaScript/Aula 05 - Operadores.js
@@ -0,0 +1,49 @@
+x = 53;
+y = 24;
+
+soma = x + y;
+subtracao = x - y;
+multiplicacao = x * y;
+quociente = x / y;
+resto = x % y;
+potencia = x ** y;
+
+console.log("_____________________________________________");
+console.log("Operadores Aritméticos")
+console.log("x =",x, "|", "y =",y);
+console.log("Soma =", soma);
+console.log("subtracao =", subtracao);
+console.log("multiplicacao =", multiplicacao);
+console.log("quociente =", quociente);
+console.log("resto =", resto);
+console.log("potencia =", potencia);
+console.log("_____________________________________________");
+
+console.log("_____________________________________________");
+console.log("Operadores Relacionais");
+a = 10;
+b = 20;
+c = "10";
+console.log("a =", a, "b =", b, "c =", c);
+console.log("a == c:", a == c);
+console.log("a != c:", a != c);
+console.log("a === c:", a === c);
+console.log("a !== c:", a !== c);
+console.log("a < b:", a < b);
+console.log("a > b:", a > b);
+console.log("a <= b:", a <= b);
+console.log("a >= b:", a >= b);
+console.log("_____________________________________________");
+
+console.log("_____________________________________________");
+console.log("Operadores Lógicos");
+e = true;
+f = false;
+console.log("e =",e, "|", "f =",f);
+console.log("e || f:", e || f);
+console.log("e && f:", e && f);
+console.log("! e", !e);
+console.log("_____________________________________________");
+
+
+
