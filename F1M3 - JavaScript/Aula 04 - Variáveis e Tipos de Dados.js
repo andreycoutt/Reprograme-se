dcc10691edc074764963bdb299c620b836359451
andreycoutt <contato.andreyyy@gmail.com>
2023-09-24 19:01:50 -0300

    Variáveis e Tipos de Dados

---
+++ F1M3 - JavaScript/Aula 04 - Variáveis e Tipos de Dados.js
@@ -0,0 +1,9 @@
+nome = "teste";
+idade = 20;
+altura = 1.80;
+
+pc = "dell 15 3000";
+custo = 4030.87;
+
+console.log("nome:", nome, "idade:", idade, "altura:", altura);
+console.log("\ncomputador", pc, "custou:", custo);
