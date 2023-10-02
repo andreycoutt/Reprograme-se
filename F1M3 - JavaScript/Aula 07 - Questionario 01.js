f5fcfe748059058a86d226cac3c27f5c549f7fc6
andreycoutt <contato.andreyyy@gmail.com>
2023-10-02 18:39:57 -0300

    Questionário - Aula 07

---
+++ F1M3 - JavaScript/Aula 07 - Questionario 01.js
@@ -0,0 +1,19 @@
+var n1 = 1;
+var n2 = 2;
+var n3 = 3;
+if (n3 % n1 >= 3) {
+  console.log("Entrou no if");
+} else if (n3 % n2 >= 2){
+  console.log("Entrou no else if");
+} else{
+  console.log("Entrou no else");
+}var n1 = 1;
+var n2 = 2;
+var n3 = 3;
+if (n3 % n1 >= 3) {
+  console.log("Entrou no if");
+} else if (n3 % n2 >= 2){
+  console.log("Entrou no else if");
+} else{
+  console.log("Entrou no else");
+}
---
+++ F1M3 - JavaScript/Aula 07 - Questionario 02.js
@@ -0,0 +1,7 @@
+var n1 = 1;
+var n2 = 2;
+if (n1 + n2 >= 3) {
+  console.log("A soma é maior ou igual a três");
+} else {
+  console.log("A soma é menor que três");
+}
