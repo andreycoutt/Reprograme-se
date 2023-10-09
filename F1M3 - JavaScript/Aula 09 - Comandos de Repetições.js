a7d64dc7532aac63804a8c187e53b798d012f7e8
andreycoutt <contato.andreyyy@gmail.com>
2023-10-08 22:14:10 -0300

    Comandos de Repetições

---
+++ F1M3 - JavaScript/Aula 09 - Comandos de Repetições.js
@@ -0,0 +1,21 @@
+for (i = 0; i <= 10; i++) {
+    console.log(i * 3);
+  }
+
+  console.log("______________");
+
+  j = 1;
+
+  while(j <= 10){
+    console.log(j * 3);
+    j++;
+  }
+
+  console.log("______________");
+
+  k = 1;
+
+  do{
+    console.log(k * 3);
+    k++;
+  }while(k <= 10);
