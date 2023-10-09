ab4b82e3680b22fa5f12453b13d6134865b24da9
andreycoutt <contato.andreyyy@gmail.com>
2023-10-09 00:09:38 -0300

    Questionário - Aula 10.

---
+++ F1M3 - JavaScript/Aula 10 - Questionario 01.js
@@ -0,0 +1,10 @@
+x = 1;
+cont = 0;
+while (x < 3) {
+  y = 0;
+  while (y <= 4) {
+    console.log("Instrução");
+    y = y + 1;
+  }
+  x = x + 1;
+}
---
+++ F1M3 - JavaScript/Aula 10 - Questionario 02.js
@@ -0,0 +1,6 @@
+for (x = 2; x >= 0; x = x - 1) {
+    for (y = 0; y <= 4; y = y - 1) {
+      console.log("Instrução");
+    }
+  }
+  
