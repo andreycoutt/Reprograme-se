895b55a522af04c661ec724bbb749b81eb290441
andreycoutt <contato.andreyyy@gmail.com>
2023-10-08 23:36:55 -0300

    Comandos de Repetição Aninhados

---
+++ F1M3 - JavaScript/Aula 10 - Comandos de Repetição Aninhados.js
@@ -0,0 +1,11 @@
+for(var i = 1; i <= 10; i++){
+    console.log("Divisorees do número ", i);
+    for(var j = 1; j <= i; j++){
+        if(i % j == 0){
+            console.log(j);
+        }
+    }
+}
+
+
+
