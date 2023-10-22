8801c27f15c76a5c39a8f7ca8d192496885739bf
andreycoutt <contato.andreyyy@gmail.com>
2023-10-22 20:39:04 -0300

    Funções

--- F1M3 - JavaScript/Aula 12 - Funções.js
+++ F1M3 - JavaScript/Aula 12 - Funções.js
@@ -1,16 +1,33 @@
+const { Z_RLE } = require("zlib");
+
 function mostrar_mensagem(){
-    console.log("Mensagem dentro da funçao");
-  }
-  
-  function calcular_produto(x, y, z){
-    console.log("Produto:", x*y*z);
-  }
-  
-  function calcular_quociente(a, b){
-    return a / b;
-  }
-  
-  mostrar_mensagem();
-  calcular_produto(4, 7.5, 2.3);
-  q = calcular_quociente(11, 5);
-  console.log(q);
+  console.log("Mensagem dentro da funçao");
+}
+
+function calcular_produto(x, y, z){
+  console.log("Produto:", x*y*z);
+}
+
+function calcular_quociente(a, b){
+  return a / b;
+}
+
+mostrar_mensagem();
+calcular_produto(4, 7.5, 2.3);
+q = calcular_quociente(11, 5);
+console.log(q);
+
+function calcular_area(x, y, z = 1){
+  area = x * y * z;
+  return area;
+}
+
+raio = 2;
+comprimento = 6;
+largura = 9;
+
+area_circulo = calcular_area(raio, raio, 3.14);
+console.log("Circulo", "\nRaio:", raio, "\nÁrea:", area_circulo);
+
+area_retangulo = calcular_area(comprimento, largura);
+console.log("Retângulo", "\nComprimento:", comprimento, "\nLargura:", largura, "\nÁrea:", area_retangulo);
