951c91dabd0fc76540540fb21ee6ce3da6b78bfb
andreycoutt <contato.andreyyy@gmail.com>
2023-10-22 20:22:17 -0300

    .

---
+++ F1M3 - JavaScript/Aula 12 - Funções.js
@@ -0,0 +1,11 @@
+function mostrar_mensagem(){
+    console.log("Mensagem dentro da funçao")
+  }
+  
+  function calcular_produto(x, y, z){
+    console.log("Produto:", x*y*z)
+  }
+
+  function calcular_quociente(a, b){
+    return a / b;
+  }
