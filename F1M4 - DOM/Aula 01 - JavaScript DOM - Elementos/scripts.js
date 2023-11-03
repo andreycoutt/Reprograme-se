b27c30f783daa350824f7eb60cbbf8690afb6418
andreycoutt <contato.andreyyy@gmail.com>
2023-11-03 15:33:52 -0300

    .

---
+++ F1M4 - DOM/Aula 01 - JavaScript DOM - Elementos/scripts.js
@@ -0,0 +1,12 @@
+document.write("Ola mundo");
+document.write(Date());
+document.write(document.title);
+document.write(document.characterSet);
+
+var corpo = document.body;
+corpo.style.background = "Blue";
+
+
+// getElementById
+var h1_Brasil = document.getElementById("pais_brasil");
+h1_Brasil.style.background = "Yellow";
