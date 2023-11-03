a4104a23e02ccdf07d7bb2e782379c76c8c389d5
andreycoutt <contato.andreyyy@gmail.com>
2023-11-03 15:45:38 -0300

    .

--- F1M4 - DOM/Aula 01 - JavaScript DOM - Elementos/scripts.js
+++ F1M4 - DOM/Aula 01 - JavaScript DOM - Elementos/scripts.js
@@ -11,4 +11,18 @@ corpo.style.background = "Gray";
 var h1_Brasil = document.getElementById("pais_brasil");
 h1_Brasil.style.background = "Yellow";
 document.write("<p> innerText: " + h1_Brasil.innerText);
-document.write("<p> innerHTML: " + h1_Brasil.innerHTML) 
+document.write("<p> innerHTML: " + h1_Brasil.innerHTML);
+
+
+//getElementByTagName
+var array_h2 = document.getElementsByTagName("h2");
+var h2_es = array_h2[0];
+var h2_mg =  array_h2[1];
+h2_es.style.color = "Pink";
+h2_mg.style.color = "DarkRed";
+
+
+//getElementByName
+var array_cidade = document.getElementsByName("cidade");
+var cidade_0 =  array_cidade[0];
+cidade_0.style.borderLeftColor = "Yellow";
