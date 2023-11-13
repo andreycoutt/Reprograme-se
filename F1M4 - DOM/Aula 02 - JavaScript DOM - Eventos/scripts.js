8437c2def31f0674e3cc8eb5b87f70fbef4aeffe
andreycoutt <contato.andreyyy@gmail.com>
2023-11-12 22:46:24 -0300

    .

---
+++ F1M4 - DOM/Aula 02 - JavaScript DOM - Eventos/scripts.js
@@ -0,0 +1,5 @@
+var b2 = document.getElementById("btn-2");
+b2.onclick = function(){ alert('Propriedades de manipulador de eventos');};
+
+var b3 = document.getElementById("btn-3");
+b3.addEventListener("click", function() {alert("addEventListener");}, false);
