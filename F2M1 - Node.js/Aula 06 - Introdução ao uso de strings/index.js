9707940fb8aa3197245e21de44ac588dfceca8c3
andreycoutt <contato.andreyyy@gmail.com>
2024-01-02 11:32:46 -0300

    Introdução ao uso de strings

---
+++ F2M1 - Node.js/Aula 06 - Introdução ao uso de strings/index.js
@@ -0,0 +1,12 @@
+var str = "esta é 'uma' string";
+var str2 = "esta é 'outra' string";
+
+console.log(str + " " + str2);
+
+var nome = 'andrey';
+var idade = '24';
+
+console.log("me chamo " + nome + " e tenho " + idade + " anos");
+
+
+console.log(`Olá, me chamo ${nome} e tenho ${idade} anos`);
