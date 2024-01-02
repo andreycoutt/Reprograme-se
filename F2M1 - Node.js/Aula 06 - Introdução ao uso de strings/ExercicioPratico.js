9f7150ce7923edb4c34e73d57e950a5de22d8d9a
andreycoutt <contato.andreyyy@gmail.com>
2024-01-02 11:37:32 -0300

    Exercício Prático

---
+++ F2M1 - Node.js/Aula 06 - Introdução ao uso de strings/ExercicioPratico.js
@@ -0,0 +1,8 @@
+const nome = 'Andrey';
+const idade = '24';
+const cidade = 'Rio de Janeiro';
+
+
+const Mensagem = `Olá, meu nome é ${nome}, tenho ${idade} anos e moro no ${cidade}`
+
+console.log(Mensagem);
