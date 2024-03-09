2beb345d31d8f15add17d37bb33a905c66251e2b
andreycoutt <contato.andreyyy@gmail.com>
2024-03-09 16:13:24 -0300

    Criando as rotas e o create

--- F2M3 - Persistência de dados com Sequelize/Aula 08 ~ 09 ~10 - Criando o model cliente da aplicação/index.js
+++ F2M3 - Persistência de dados com Sequelize/Aula 08 ~ 09 ~10 - Criando o model cliente da aplicação/index.js
@@ -1,12 +1,33 @@
-(async()=>{
-    const database = require('./db');
-    const Cliente = require('./cliente');
-    try{
-        const resultado = await database.sync();
-        console.log(resultado);
-    }catch(error){
-        console.log(error);
-    }
+// Criando o servidor
+const express = require('express');
+const app = express();
+const porta = 9443;
+// Definindo rotas
+app.get("/", (req, res) => {
+  res.send("Bem vindo ao cadastro de clientes.");
+});
+app.listen(porta, () => {
+  console.log("Servidor rodando");
+});
 
-})
-();   
+(async () => {
+  const database = require("./db");
+  const Cliente = require("./models/cliente");
+  try {
+    const resultado = await database.sync();
+    console.log(resultado);
+  } catch (error) {
+    console.log(error);
+  }
+})(); 
+
+(async () => {
+  const database = require("./db");
+  const Cliente = require("./models/cliente");
+  try {
+    const resultado = await database.sync();
+    console.log(resultado);
+  } catch (error) {
+    console.log(error);
+  }
+})();
